13f9cd1d102302517c5af16963398f824b881138
Sean Halstead <smh14h@acu.edu>
2019-03-08 08:50:09 -0600

    finished

--- JSONCodableExample/ViewController.swift
+++ JSONCodableExample/ViewController.swift
@@ -8,11 +8,33 @@
 
 import UIKit
 
+struct Country:Decodable {
+    let name: String
+    let capital: String
+    let region: String
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = "https://restcountries.eu/rest/v2/all"
+        let urlObj = URL(string: url)
+        
+        URLSession.shared.dataTask(with: urlObj!) {( data, response, error) in
+            
+            do {
+                var countries = try JSONDecoder().decode([Country].self, from: data!)
+                
+                for country in countries {
+                    print(country.name + ":" + country.capital)
+                }
+            } catch {
+                print("Error Here")
+            }
+        }.resume()
     }
 
 
