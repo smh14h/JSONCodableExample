fbe2cab65a1b588bd89408da5a796c8326975e99
Sean Halstead <smh14h@acu.edu>
2019-03-08 08:27:14 -0600

    Initial Commit

---
+++ JSONCodableExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONCodableExample
+//
+//  Created by Sean Halstead on 3/8/19.
+//  Copyright © 2019 Sean Halstead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
